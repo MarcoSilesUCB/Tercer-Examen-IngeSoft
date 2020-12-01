2cfc49b716aa59ef710f8c56765bb9318a12829c
MarcoSilesUCB <marco.siles@ucb.edu.bo>
2020-12-01 09:32:03 -0400

    calcular precio final (3) v.1.3

--- lib/ventaProdu.rb
+++ lib/ventaProdu.rb
@@ -1,6 +1,28 @@
 class VentaProdu
     
     def calcular_precio_final(cantidad_items, precio_unitario, estado)
-        return precio_unitario-(precio_unitario*0.06)
+        precioF=0
+        if(estado == "UT")
+            precioF=precioF+precio_unitario+(precio_unitario*0.06)
+        else
+            if(estado == "NV")
+                precioF=precioF+precio_unitario+(precio_unitario*0.08)
+            else
+                if(estado == "TX")
+                    precioF=precioF+precio_unitario+(precio_unitario*0.065)
+                else
+                    if(estado == "AL")
+                        precioF=precioF+precio_unitario+(precio_unitario*0.04)
+                    else
+                        if(estado == "CA")
+                            precioF=precioF+precio_unitario+(precio_unitario*0.03)
+                        end
+                    end
+                end
+            end
+        end
+
+                
+        return precioF
     end
 end
--- spec/ventaProdu_spec.rb
+++ spec/ventaProdu_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe VentaProdu do
     it "calcular precio final (1)" do
         ventaProducto = VentaProdu.new
         precio = ventaProducto.calcular_precio_final(1,100,"UT")
-        expect(precio).to eq(94)
+        expect(precio).to eq(106)
     end
 
     it "calcular precio final (2)" do
         ventaProducto = VentaProdu.new
         precio = ventaProducto.calcular_precio_final(1,200,"UT")
-        expect(precio).to eq(188)
+        expect(precio).to eq(212)
+
+    end
+
+    it "calcular precio final (3)" do
+        ventaProducto = VentaProdu.new
+        precio = ventaProducto.calcular_precio_final(1,200,"NV")
+        expect(precio).to eq(216)
 
     end
 
